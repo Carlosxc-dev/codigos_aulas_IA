d0c5667009e7f7bcc47a9a4f1febfc7ab282c458
carlos_henrique <chcruz64@gmail.com>
2024-04-30 20:15:02 -0300

    up

--- ia_disfagia_p4_02.py
+++ ia_disfagia_p4_02.py
@@ -1,8 +1,13 @@
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 import os
 import pandas as pd
 import librosa
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 import glob
 import numpy as np
@@ -32,39 +37,46 @@ for b in tqdm(bons):
         linha.append((x[i:max], y[i:max]))
     dados.append(linha)
 
-# plotar os dados
+# Plotar os dados de áudio
 # fig, ax = plt.subplots(len(dados), 1, figsize=(15, 3*len(dados)))
 # for i, linha in enumerate(dados):
 #     ax[i].set_title(bons[i])
 #     for x, y in linha:
-#         ax[i].plot(x, y)
+#         ax[i].plot(x, y, color='blue')
+#         for intervalo in range(0, len(x), int(passo)):
+#             ax[i].axvline(x=x[intervalo], color='red',
+#                           linestyle='--', linewidth=0.5)
+# plt.tight_layout()
 # plt.show()
 
-
-# aplicando o modelo de detecção de anomalias
+# --------------------------------------------------------------
 X = []
+y = []
 for i, linha in enumerate(dados):
-    for x, y in linha:
-        X.append(y)
+    for x, audio in linha:
+        X.append(audio)
+        y.append(-1)  # -1 para anomalia
 
-# Transformar a lista de limites em um array numpy bidimensional
-X = np.array(X)
-
-Xp = MinMaxScaler().fit_transform(X)
-skb = SelectKBest(chi2, k=50)
-X = skb.fit_transform(Xp, y)
-del Xp
-X.shape, skb.get_params()
-
-
+# Criar e carregar o classificador RandomForest
 clf = joblib.load('RandomForestClassifier.joblib')
-ano = clf.predict(X)
-ano = (ano == -1)
+clf.fit(X, y)
 
-fig, ax = plt.subplots(1, 1, figsize=(15, 3*len(dados)))
-n = -1
+# Fazer previsões e ajustar os valores para -1 (anomalia) e 1 (não anomalia)
+predictions = clf.predict(X)
+predictions = np.where(predictions == -1, -1, 1)
+
+# Plotar os dados de áudio
+# Plotar os dados de áudio
+fig, ax = plt.subplots(len(dados), 1, figsize=(15, 3*len(dados)))
 for i, linha in enumerate(dados):
+    ax[i].set_title(bons[i])
     for x, y in linha:
-        n += 1
-        ax.plot(x, y, color='black' if ano[n] else 'gray')
+        ax[i].plot(x, y, color='blue')  # Plotar os dados de áudio em azul
+        for intervalo, anomalia in zip(range(0, len(x) - janela, passo), predictions):
+            if anomalia == -1:  # Se uma anomalia for identificada
+                inicio = int(intervalo)
+                fim = int(intervalo + janela)
+                ax[i].axvspan(x[inicio], x[fim], color='red',
+                              alpha=0.3)  # Corrigindo a coloração
+plt.tight_layout()
 plt.show()
